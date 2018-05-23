bc8ea36f686cee1e1d7c20747533879db34f56ca
EpicLagg <horvathorvath.horvat@gmail.com>
2018-05-23 18:57:03 +0200

    Add files via upload

--- KompresijaTeksta.c
+++ KompresijaTeksta.c
@@ -20,5 +20,54 @@ int main()
 	int duljina = longestline(f) + 2;
     char tekst[duljina];
     fgets(tekst, duljina, f);
-    puts(tekst);
+    int broj_rijeci = 1;
+    //pronalazenje broja rijeci u tekstu
+    for(int i = 0; i < duljina; i++) {
+        if(tekst[i] == ' ')
+            broj_rijeci++;
+    }
+    //pretvaranje teksta u niz rijeci
+    char rijeci[broj_rijeci][32], buffer[32], *p = tekst;
+    for(int i = 0; i < broj_rijeci - 1; i++) {
+		size_t duljinarijeci = strlen(p) - strlen(strchr(p, ' '));
+		memcpy(buffer, p, duljinarijeci);
+		buffer[duljinarijeci] = '\0';
+		memmove(rijeci[i], buffer, duljinarijeci + 1);
+		p = strchr(p, ' ') + 1;
+    }
+    p = strrchr(tekst, ' ') + 1;
+    memcpy(buffer, p, strlen(p));
+    buffer[strlen(p)] = '\0';
+	memmove(rijeci[broj_rijeci - 1], buffer, strlen(p) + 1);
+	//test
+	for(int i = 0; i < broj_rijeci; i++) {
+		puts(rijeci[i]);
+	}
+	//"kodiranje"
+	char kodiranerijeci[broj_rijeci][32];
+	int bk = 0;
+	fprintf(k, "%d\n", broj_rijeci);
+	for(int i = 0; i < broj_rijeci; i++) {
+		int razlika = 0;
+		for(int ii = 0; ii < i; ii++) {
+			if(strcmp(rijeci[i], rijeci[ii]) != 0) {
+				razlika++;
+			}
+		}
+		if(razlika == i) {
+			memmove(kodiranerijeci[bk], rijeci[i], strlen(rijeci[i]) + 1);
+			bk++;
+			fprintf(k, "%s ", rijeci[i]);
+		}
+	}
+	printf("%d", bk);
+	/*
+	for(int i = 0; i < bk; i++) {
+		puts(rijeci[i]);
+	}
+	*/
+	//output
+	for(int i = 0; i < broj_rijeci; i++) {
+		fprintf(o, )
+	}
 }
